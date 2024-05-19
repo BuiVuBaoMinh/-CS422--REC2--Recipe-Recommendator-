151d153bf513ab4917aba781ce8edf023e34128c
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2024-05-19 12:49:54 +0700

    Update Scan Ingredients Page
    - Camera preview is no full-screen.
    - Take photo button is repositioned for better access.

--- lib/camera_page/camera_page.dart
+++ lib/camera_page/camera_page.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ScanIngredientsPage extends StatefulWidget {
 
@@ -31,7 +32,8 @@ class ScanIngredientsPageState extends State<ScanIngredientsPage> {
     // --> create one
     _controller = CameraController(
       widget.camera, 
-      ResolutionPreset.medium,);
+      ResolutionPreset.ultraHigh,
+      enableAudio: false);
 
     // Initialize the controller.
     _initializeControllerFuture = _controller.initialize();
@@ -46,44 +48,75 @@ class ScanIngredientsPageState extends State<ScanIngredientsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Scan your ingredients!'),),
-        body:
-          // Must wait for the controller to finish initializing
-          FutureBuilder<void>(
-            future: _initializeControllerFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                // Future is complete, display preview
-                return CameraPreview(_controller);
-              } else {
-                // Else display loading indicator
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
-          floatingActionButton: FloatingActionButton (
-            onPressed: () async {
-              try {
-                await _initializeControllerFuture;
+    // Must wait for the controller to finish initializing
+    return FutureBuilder<void>(
+      future: _initializeControllerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // Future is complete, display preview
+          // return CameraPreview(_controller);
+          final mediaSize = MediaQuery.of(context).size;
+          final scale = 1 / (_controller.value.aspectRatio * mediaSize.aspectRatio);
 
-                final image = await _controller.takePicture();
-
-                if (!context.mounted) return;
-
-                await Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => DisplayPictureScreen(imagePath: image.path),
+          return SafeArea(
+            child: Stack(
+              children: [
+                ClipRect(
+                  clipper: _MediaSizeClipper(mediaSize),
+                  child: Transform.scale(
+                    scale: scale,
+                    alignment: Alignment.topCenter,
+                    child: CameraPreview(_controller),
                   ),
-                );
-              } catch (e) {
-                print(e);
-              }
-            },
-            child: const Icon(Icons.camera_alt),),
-      )
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: FloatingActionButton (
+                            onPressed: () async {
+                              try {
+                                await _initializeControllerFuture;
+                        
+                                final image = await _controller.takePicture();
+                        
+                                if (!context.mounted) return;
+                        
+                                await Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => DisplayPictureScreen(imagePath: image.path),
+                                  ),
+                                );
+                              } catch (e) {
+                                print(e);
+                              }
+                            },
+                            child: const Icon(Icons.camera_alt)
+                    ),
+                  ),
+                ),
+              ]
+            )
+          );
+        } else {
+          // Else display loading indicator
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
+  }
+}
+
+class _MediaSizeClipper extends CustomClipper<Rect> {
+  final Size mediaSize;
+  const _MediaSizeClipper(this.mediaSize);
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromLTWH(0, 0, mediaSize.width, mediaSize.height);
+  }
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) {
+    return true;
   }
 }
 
